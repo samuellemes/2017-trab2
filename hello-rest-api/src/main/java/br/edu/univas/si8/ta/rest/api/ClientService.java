c14ac052a8c590b12c9d98a3d6bd5cddde530df8
Samuel Lemes <samuellemes25@gmail.com>
2017-11-13 20:55:13 -0200

    Creating ClientService and persistence.xml.

---
+++ hello-rest-api/src/main/java/br/edu/univas/si8/ta/rest/api/ClientService.java
@@ -0,0 +1,22 @@
+package br.edu.univas.si8.ta.rest.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("service")
+public interface ClientService {
+	
+	@GET
+	@Path("/clients/names")
+	@Produces(MediaType.APPLICATION_JSON)
+	String[] listProductNames();
+	
+	@POST
+	@Path("/clients/{name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	String saveNewProduct(@PathParam("name") String name);
+}
